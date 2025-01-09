a7b1006fe656b32727e7af7e5673db6db30d9ccc
Wen Zhang <wenzhang.a12@gmail.com>
2025-01-09 11:41:02 -0500

    inital starting code

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+import sqlite3
+import os
+from flask import Flask, render_template, request, session, redirect
+
+
+
+
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['GET', 'POST'])
+def landing():
+    return render_template("main.html")
+
+
+
