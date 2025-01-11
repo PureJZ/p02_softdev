16ad5ba2476dafd4a91a46fca2fd74c4700df731
Jackie Zeng <jzeng246@gmail.com>
2025-01-11 14:30:25 -0500

    create minesweeper page (not loading game)

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, Blueprint
+
 
 
 
@@ -11,6 +12,10 @@ app = Flask(__name__)
 @app.route("/", methods = ['GET', 'POST'])
 def landing():
     return render_template("main.html")
+
+@app.route('/minesweeper')
+def minesweeper():
+    return render_template('minesweeper.html')
 
 if __name__ == "__main__":
     app.debug = True
